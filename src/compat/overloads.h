88f1625f1d75343040b19bb87603121344fe130f
Davood <dd1424@msstate.edu>
2026-03-05 09:18:42 -0600

    final piece

--- src/compat/overloads.h
+++ src/compat/overloads.h
@@ -1,161 +1,149 @@
 #ifndef OVERLOADS_H
 #define OVERLOADS_H
 
-#include <vector>
-#include "particle/particle2.h"
 #include "debug/err.h"
+#include "particle/particle3.h"
+#include <vector>
 
 using namespace std;
 
 // print a vector of anything
-template <typename ttype>
-ostream& operator << (ostream& o, vector<ttype> v){
-    size_t N = v.size();
-    for (size_t i = 0; i < N; ++i) {
-	o << "[" << v[i] << "]";
-    }
-    return o;
+template <typename ttype> ostream &operator<<(ostream &o, vector<ttype> v) {
+  size_t N = v.size();
+  for (size_t i = 0; i < N; ++i) {
+    o << "[" << v[i] << "]";
+  }
+  return o;
 };
 
 // Vector and scaler operation
 template <typename ttype, typename t2>
-vector<ttype> & operator -= (vector<ttype> &v1, t2 v2){
-	for (unsigned i = 0; i < v1.size(); ++i) {
-	    v1[i] -= v2;
-	}
-	return v1;
+vector<ttype> &operator-=(vector<ttype> &v1, t2 v2) {
+  for (unsigned i = 0; i < v1.size(); ++i) {
+    v1[i] -= v2;
+  }
+  return v1;
 };
 template <typename ttype, typename t2>
-vector<ttype> & operator += (vector<ttype> &v1, t2 v2){
-	for (unsigned i = 0; i < v1.size(); ++i) {
-	    v1[i] += v2;
-	}
-	return v1;
+vector<ttype> &operator+=(vector<ttype> &v1, t2 v2) {
+  for (unsigned i = 0; i < v1.size(); ++i) {
+    v1[i] += v2;
+  }
+  return v1;
 };
 template <typename ttype, typename t2>
-vector<ttype> & operator *= (vector<ttype> &v1, t2 v2){
-	for (unsigned i = 0; i < v1.size(); ++i) {
-	    v1[i] *= v2;
-	}
-	return v1;
+vector<ttype> &operator*=(vector<ttype> &v1, t2 v2) {
+  for (unsigned i = 0; i < v1.size(); ++i) {
+    v1[i] *= v2;
+  }
+  return v1;
 };
 template <typename ttype, typename t2>
-vector<ttype> & operator /= (vector<ttype> &v1, t2 v2){
-	for (unsigned i = 0; i < v1.size(); ++i) {
-	    v1[i] /= v2;
-	}
-	return v1;
+vector<ttype> &operator/=(vector<ttype> &v1, t2 v2) {
+  for (unsigned i = 0; i < v1.size(); ++i) {
+    v1[i] /= v2;
+  }
+  return v1;
 };
-
 
 // Vector and vector operation
-// Addition of vector of objects, isn't it obvious? Now we need to define _every_ type of point wise operator :(
+// Addition of vector of objects, isn't it obvious? Now we need to define
+// _every_ type of point wise operator :(
 template <typename ttype>
-vector<ttype> operator + (vector<ttype> v1, vector<ttype> v2){
-    int N1 = v1.size();
-    int N2 = v2.size();
+vector<ttype> operator+(vector<ttype> v1, vector<ttype> v2) {
+  int N1 = v1.size();
+  int N2 = v2.size();
 
-    if (N1 == N2) {
-	vector<ttype> v3(N1);
-	for (int i = 0; i < N1; ++i) {
-	    v3[i] = v1[i] + v2[i];
-	}
-	return v3;
+  if (N1 == N2) {
+    vector<ttype> v3(N1);
+    for (int i = 0; i < N1; ++i) {
+      v3[i] = v1[i] + v2[i];
     }
-    else 
-    {
-	//errout("Incompatible dimension.");
-	terminate();
-    }
+    return v3;
+  } else {
+    // errout("Incompatible dimension.");
+    terminate();
+  }
 };
 
 template <typename ttype>
-vector<ttype> operator - (vector<ttype> v1, vector<ttype> v2){
-    int N1 = v1.size();
-    int N2 = v2.size();
+vector<ttype> operator-(vector<ttype> v1, vector<ttype> v2) {
+  int N1 = v1.size();
+  int N2 = v2.size();
 
-    if (N1 == N2) {
-	vector<ttype> v3(N1);
-	for (int i = 0; i < N1; ++i) {
-	    v3[i] = v1[i] - v2[i];
-	}
-	return v3;
+  if (N1 == N2) {
+    vector<ttype> v3(N1);
+    for (int i = 0; i < N1; ++i) {
+      v3[i] = v1[i] - v2[i];
     }
-    else {
-	errout("Incompatible dimension.");
-	terminate();
-    }
+    return v3;
+  } else {
+    errout("Incompatible dimension.");
+    terminate();
+  }
 };
 
 template <typename ttype>
-vector<ttype> operator * (vector<ttype> v1, vector<ttype> v2){
-    int N1 = v1.size();
-    int N2 = v2.size();
+vector<ttype> operator*(vector<ttype> v1, vector<ttype> v2) {
+  int N1 = v1.size();
+  int N2 = v2.size();
 
-    if (N1 == N2) {
-	vector<ttype> v3(N1);
-	for (int i = 0; i < N1; ++i) {
-	    v3[i] = v1[i] * v2[i];
-	}
-	return v3;
+  if (N1 == N2) {
+    vector<ttype> v3(N1);
+    for (int i = 0; i < N1; ++i) {
+      v3[i] = v1[i] * v2[i];
     }
-    else 
-    {
-	errout("Incompatible dimension.");
-	terminate();
-    }
+    return v3;
+  } else {
+    errout("Incompatible dimension.");
+    terminate();
+  }
 };
 
 template <typename ttype>
-vector<ttype> operator / (vector<ttype> v1, vector<ttype> v2){
-    int N1 = v1.size();
-    int N2 = v2.size();
+vector<ttype> operator/(vector<ttype> v1, vector<ttype> v2) {
+  int N1 = v1.size();
+  int N2 = v2.size();
 
-    if (N1 == N2) {
-	vector<ttype> v3(N1);
-	for (int i = 0; i < N1; ++i) {
-	    v3[i] = v1[i] / v2[i];
-	}
-	return v3;
+  if (N1 == N2) {
+    vector<ttype> v3(N1);
+    for (int i = 0; i < N1; ++i) {
+      v3[i] = v1[i] / v2[i];
     }
-    else 
-    {
-	errout("Incompatible dimension.");
-	terminate();
-    }
+    return v3;
+  } else {
+    errout("Incompatible dimension.");
+    terminate();
+  }
 };
 
 // Unary vector operators
 template <typename ttype>
-vector<ttype> & operator += (vector<ttype> &v1, vector<ttype> v2){
-    int N1 = v1.size();
-    int N2 = v2.size();
+vector<ttype> &operator+=(vector<ttype> &v1, vector<ttype> v2) {
+  int N1 = v1.size();
+  int N2 = v2.size();
 
-    if (N1 == N2) {
-	vector<ttype> v3(N1);
-	for (int i = 0; i < N1; ++i) {
-	    v1[i] += v2[i];
-	}
-	return v1;
+  if (N1 == N2) {
+    vector<ttype> v3(N1);
+    for (int i = 0; i < N1; ++i) {
+      v1[i] += v2[i];
     }
-    else 
-    {
-	errout("Incompatible dimension.");
-	terminate();
-    }
+    return v1;
+  } else {
+    errout("Incompatible dimension.");
+    terminate();
+  }
 };
-
 
 // scalar and vector operation
 
-template <typename ttype>
-vector<ttype> operator * (double s, vector<ttype> v1){
-    unsigned N1 = v1.size();
-    vector<ttype> v3(N1);
-    for (unsigned i = 0; i < N1; ++i) {
-	v3[i] = s * v1[i];
-    }
-    return v3;
+template <typename ttype> vector<ttype> operator*(double s, vector<ttype> v1) {
+  unsigned N1 = v1.size();
+  vector<ttype> v3(N1);
+  for (unsigned i = 0; i < N1; ++i) {
+    v3[i] = s * v1[i];
+  }
+  return v3;
 };
 
 #endif /* ifndef OVERLOADS_H */
